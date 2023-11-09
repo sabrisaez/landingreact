7fad2ad202a0356009c5f1ac74063529e312d4a1
Sabrina <sabrinasaeztoro@gmail.com>
2023-11-09 00:06:48 -0300

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { GlobalStyles } from "./styles/globalstyles.js";
+import { GlobalStyles } from "./styles/global_styles.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
