e3d6ab94dc7bad62136a73e8fe11b03145ff39b0
Sabrina <sabrinasaeztoro@gmail.com>
2023-11-09 19:01:56 -0300

    products update

--- src/components/products/product_card/product_card.jsx
+++ src/components/products/product_card/product_card.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { ProductoCard } from "./styles";
 import { Button } from "../../button/button";
 
-export const ProductCard = ({ img, tittle, desc, price }) => {
+export const ProductCard = ({ img, title, desc, price }) => {
   return (
     <ProductoCard>
       <img src={img} alt="" />
-      <h2>{tittle}</h2>
+      <h2>{title}</h2>
       <p>{desc}</p>
       <div>
         <p>${price}</p>
--- src/components/products/product_card/styles.js
+++ src/components/products/product_card/styles.js
@@ -1,21 +1,24 @@
 import styled from "styled-components";
 
 export const ProductoCard = styled.div`
-  background: var(--gray-bg);
-  width: 300px;
+  background: #aee9d1;
+  width: 250px;
   border-radius: 15px;
   padding: 1rem;
   img {
-    width: 100%;
+    width: 95%;
     margin-bottom: 1rem;
+    border-radius: 1rem;
   }
   h2 {
     font-weight: 600;
     margin: 0;
+    color: #7fa37a;
+    font-size: 0.8rem;
   }
   p {
     color: #666;
-    font-size: 1.2rem;
+    font-size: 0.7rem;
   }
   @media (max-width: 768px) {
     width: 250px;
