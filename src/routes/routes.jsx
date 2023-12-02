3b898719e76a6eaa3c1b9ce2e8de8be560c3fee2
Sabrina <sabrinasaeztoro@gmail.com>
2023-12-01 22:25:52 -0300

    update

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomeLayout } from "../layout/home/home_layout";
-import { Home } from "../pages/Home/Home";
+import { Home } from "../pages/home/home";
 import { AboutUs } from "../pages/about/about";
 import { ProductsPage } from "../pages/products/productspage";
 import { Contactpage } from "../pages/contact/contactpage";
